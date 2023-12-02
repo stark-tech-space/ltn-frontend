798753187d4162ae4103b923cd38b4bbae8f0f90
王石川 <gavin@192.168.1.7>
2023-12-03 01:15:16 +0800

    Changed: 調整註冊chartjs

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,15 @@
 import { RecoilRoot } from "recoil";
 import Router from "./router";
-import {
-  Chart as ChartJS,
-  LinearScale,
-  CategoryScale,
-  BarElement,
-  PointElement,
-  LineElement,
-  Legend,
-  Tooltip,
-  LineController,
-  BarController,
-  TimeScale,
-  TimeSeriesScale,
-} from "chart.js";
-import "chartjs-adapter-moment";
-
-ChartJS.register(
-  LinearScale,
-  CategoryScale,
-  BarElement,
-  PointElement,
-  LineElement,
-  Legend,
-  Tooltip,
-  LineController,
-  BarController,
-  TimeScale,
-  TimeSeriesScale
-);
+import Register from "Register";
 
 function App() {
   return (
-    <RecoilRoot>
-      <Router />
-    </RecoilRoot>
+    <>
+      <RecoilRoot>
+        <Router />
+      </RecoilRoot>
+      <Register />
+    </>
   );
 }
 
