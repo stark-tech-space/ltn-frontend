9dadb2ab65e03c37c1c06a6157f453067cb17103
Xuan0410 <b06902110@ntu.edu.tw>
2023-12-07 00:31:38 +0800

    feat: add weekly turnover ability

--- src/page/Profitability/component/WeeklyTurnoverAbility/GrapConfig.ts
+++ src/page/Profitability/component/WeeklyTurnoverAbility/GrapConfig.ts
@@ -1,43 +1,126 @@
-export const TURNOVER_DATASETS = {
-  labels: [],
-  datasets: [
-    {
-      label: "應收帳款週轉",
-      data: [],
-      borderColor: "rgb(232,175,0)",
-      backgroundColor: "rgb(232,175,0)",
-      borderWidth: 1,
-      yAxisID: "y",
-      fill: false,
-    },
-    {
-      label: "存貨週轉",
-      data: [],
-      backgroundColor: "rgba(237, 88, 157, 0.15)",
-      borderColor: "rgba(237, 88, 157, 0.35)",
-      borderWidth: 1,
-      yAxisID: "y",
-      fill: false,
-    },
-  ],
+export const TURNOVER_DATASETS: Record<string, any> = {
+  "0": {
+    labels: ["應收帳款週轉", "存貨週轉"],
+    datasets: [
+      {
+        type: "line" as const,
+        label: "應收帳款週轉",
+        data: [],
+        borderColor: "rgb(232,175,0)",
+        backgroundColor: "rgb(232,175,0)",
+        borderWidth: 1,
+        yAxisID: "y",
+        fill: false,
+      },
+      {
+        type: "line" as const,
+        label: "存貨週轉",
+        data: [],
+        backgroundColor: "rgba(237, 88, 157, 0.15)",
+        borderColor: "rgba(237, 88, 157, 0.35)",
+        borderWidth: 1,
+        yAxisID: "y",
+        fill: false,
+      },
+    ],
+  },
+  "1": {
+    labels: ["固定資產", "固定資產周轉"],
+    datasets: [
+      {
+        type: "bar" as const,
+        label: "固定資產",
+        data: [],
+        borderColor: "rgb(232,175,0)",
+        backgroundColor: "rgb(232,175,0)",
+        borderWidth: 1,
+        yAxisID: "y1",
+        fill: false,
+      },
+      {
+        type: "line" as const,
+        label: "固定資產周轉",
+        data: [],
+        backgroundColor: "rgba(237, 88, 157, 0.15)",
+        borderColor: "rgba(237, 88, 157, 0.35)",
+        borderWidth: 1,
+        yAxisID: "y",
+        fill: false,
+      },
+    ],
+  },
+  "2": {
+    labels: ["總資產", "總資產週轉"],
+    datasets: [
+      {
+        type: "bar" as const,
+        label: "總資產",
+        data: [],
+        borderColor: "rgb(232,175,0)",
+        backgroundColor: "rgb(232,175,0)",
+        borderWidth: 1,
+        yAxisID: "y1",
+        fill: false,
+      },
+      {
+        type: "line" as const,
+        label: "總資產週轉",
+        data: [],
+        backgroundColor: "rgba(237, 88, 157, 0.15)",
+        borderColor: "rgba(237, 88, 157, 0.35)",
+        borderWidth: 1,
+        yAxisID: "y",
+        fill: false,
+      },
+    ],
+  },
 };
 
-export const singleLabelDataSets = {
-  labels: [],
-  datasets: [
-    {
-      label: "所得稅佔稅前淨利比",
-      data: [],
-      borderColor: "rgb(0, 99, 132)",
-      backgroundColor: "rgb(0, 99, 132)",
-      borderWidth: 1,
-      yAxisID: "y",
-      fill: false,
+export const TURNOVER_GRAPH_Y1_AXIS = {
+  type: "linear",
+  display: true,
+  position: "right",
+  title: {
+    display: true,
+    align: "end",
+    text: "千元",
+    font: {
+      size: 12,
+      weight: "bold",
     },
-  ],
+  },
+  grid: {
+    drawOnChartArea: false,
+  },
 };
 
-export const TURNOVER_GRAPH_OPTIONS = {
+const DEFAULT_TURNOVER_GRAPH_OPTION_SCALES = {
+  x: {
+    alignToPixels: true,
+    offset: false,
+    type: "time",
+    time: {
+      unit: "year",
+      tooltipFormat: "YYYY/MM",
+    },
+  },
+  y: {
+    type: "linear",
+    display: true,
+    position: "left",
+    title: {
+      display: true,
+      text: "次",
+      align: "end",
+      font: {
+        size: 12,
+        weight: "bold",
+      },
+    },
+  },
+};
+
+const DEFAULT_TURNOVER_GRAPH_OPTIONS = {
   maintainAspectRatio: false,
   responsive: true,
   plugins: {
@@ -45,27 +128,53 @@ export const TURNOVER_GRAPH_OPTIONS = {
       position: "bottom",
     },
   },
-  scales: {
-    x: {
-      alignToPixels: true,
-      offset: false,
-      type: "time",
-      time: {
-        unit: "year",
-        tooltipFormat: "YYYY/MM",
+  scales: DEFAULT_TURNOVER_GRAPH_OPTION_SCALES,
+};
+
+export const TURNOVER_GRAPH_OPTIONS: Record<string, any> = {
+  "0": DEFAULT_TURNOVER_GRAPH_OPTIONS,
+  "1": {
+    ...DEFAULT_TURNOVER_GRAPH_OPTIONS,
+    scales: {
+      ...DEFAULT_TURNOVER_GRAPH_OPTION_SCALES,
+      y1: {
+        type: "linear",
+        display: true,
+        position: "right",
+        title: {
+          display: true,
+          align: "end",
+          text: "千元",
+          font: {
+            size: 12,
+            weight: "bold",
+          },
+        },
+        grid: {
+          drawOnChartArea: false,
+        },
       },
     },
-    y: {
-      type: "linear",
-      display: true,
-      position: "left",
-      title: {
+  },
+  "2": {
+    ...DEFAULT_TURNOVER_GRAPH_OPTIONS,
+    scales: {
+      ...DEFAULT_TURNOVER_GRAPH_OPTION_SCALES,
+      y1: {
+        type: "linear",
         display: true,
-        text: "次",
-        align: "end",
-        font: {
-          size: 12,
-          weight: "bold",
+        position: "right",
+        title: {
+          display: true,
+          align: "end",
+          text: "千元",
+          font: {
+            size: 12,
+            weight: "bold",
+          },
+        },
+        grid: {
+          drawOnChartArea: false,
         },
       },
     },
--- src/page/Profitability/component/WeeklyTurnoverAbility/index.tsx
+++ src/page/Profitability/component/WeeklyTurnoverAbility/index.tsx
@@ -1,82 +1,98 @@
-import { Stack, Box, Button, ButtonGroup } from '@mui/material';
-import { useEffect, useMemo, useState } from 'react';
-import TagCard from '../../../../component/tabCard';
+import { Stack, Box, Button } from "@mui/material";
+import { useEffect, useMemo, useRef, useState } from "react";
+import TagCard from "../../../../component/tabCard";
 
-import { AgGridReact } from 'ag-grid-react';
-import { IProfitRatio } from 'types/profitability';
+import { Chart as ReactChart } from "react-chartjs-2";
+import { AgGridReact } from "ag-grid-react";
+import numeral from "numeral";
 
-import { currentStock } from 'recoil/selector';
-import { useRecoilValue } from 'recoil';
-import { fetchProfitRatio } from 'api/profitrato';
-import { PERIOD, PERIOD_YEAR } from 'types/common';
-import numeral from 'numeral';
-import { getBeforeYears, getDataLimit } from 'until';
-import { fetchFindMindAPI } from 'api/common';
+import { IProfitRatio } from "types/profitability";
+import { PERIOD, PERIOD_YEAR } from "types/common";
+import { getDataLimit } from "until";
+
+import { currentStock } from "recoil/selector";
+import { useRecoilValue } from "recoil";
+import { fetchProfitRatio } from "api/profitrato";
+import { TURNOVER_DATASETS, TURNOVER_GRAPH_OPTIONS } from "./GrapConfig";
+import { Chart } from "chart.js";
 
 const TABLE_FIELDS: Record<string, Array<{ field: string; headerName: string }>> = {
-  '0': [
+  "0": [
     {
-      field: 'receivablesTurnover',
-      headerName: '應收帳款週轉',
+      field: "receivablesTurnover",
+      headerName: "應收帳款週轉",
     },
     {
-      field: 'inventoryTurnover',
-      headerName: '存貨週轉',
-    },
-  ],
-  '1': [
-    {
-      field: 'receivablesTurnover',
-      headerName: '固定資產',
-    },
-    {
-      field: 'fixedAssetTurnover',
-      headerName: '固定資產周轉',
+      field: "inventoryTurnover",
+      headerName: "存貨週轉",
     },
   ],
-  '2': [
+  "1": [
     {
-      field: 'TotalAssets',
-      headerName: '總資產',
+      field: "",
+      headerName: "固定資產",
     },
     {
-      field: 'assetTurnover',
-      headerName: '總資產週轉',
+      field: "fixedAssetTurnover",
+      headerName: "固定資產周轉",
+    },
+  ],
+  "2": [
+    {
+      field: "TotalAssets",
+      headerName: "總資產",
+    },
+    {
+      field: "assetTurnover",
+      headerName: "總資產週轉",
     },
   ],
 };
 
 export default function WeeklyTurnoverAbility() {
   const stock = useRecoilValue(currentStock);
+  const chartRef = useRef<Chart>();
 
   const [tabIndex, setTabIndex] = useState(0);
   const [data, setData] = useState<IProfitRatio[]>([]);
   const [period, setPeriod] = useState(3);
   const [reportType, setReportType] = useState<PERIOD>(PERIOD.QUARTER);
 
-  const handleGetAssets = async () => {
-    const rst = await fetchFindMindAPI<any>({
-      data_id: stock.No,
-      dataset: 'TaiwanStockBalanceSheet',
-      start_date: getBeforeYears(period),
-    });
-    console.log('rst', rst);
+  const handleUpdateGraph = (data: IProfitRatio[]) => {
+    if (chartRef.current) {
+      const labels = data.map((item) => item.date);
+      chartRef.current.data.labels = labels;
+
+      TABLE_FIELDS[tabIndex].forEach(async ({ field }, index) => {
+        if (chartRef.current) {
+          chartRef.current.data.datasets[index].data = data.map(
+            (item) => +item[field as keyof IProfitRatio]
+          );
+        }
+      });
+
+      chartRef.current.update();
+    }
   };
 
   useEffect(() => {
     const limit = getDataLimit(reportType, period);
     fetchProfitRatio<IProfitRatio[]>(stock.Symbol, PERIOD.QUARTER, limit).then((res) => {
       setData(res || []);
+      handleUpdateGraph(res || []);
     });
-    handleGetAssets();
   }, [stock, reportType, period]);
+
+  useEffect(() => {
+    handleUpdateGraph(data);
+  }, [tabIndex]);
 
   const columnHeaders = useMemo(() => {
     const columns: any[] = [
       {
-        field: 'title',
-        headerName: reportType === PERIOD.QUARTER ? '年度/季度' : '年度',
-        pinned: 'left',
+        field: "title",
+        headerName: reportType === PERIOD.QUARTER ? "年度/季度" : "年度",
+        pinned: "left",
       },
     ];
     data?.forEach((item) => {
@@ -98,32 +114,34 @@ export default function WeeklyTurnoverAbility() {
         data?.forEach((item) => {
           if (reportType === PERIOD.ANNUAL) {
             dataSources[item.calendarYear] = numeral(item[field as keyof IProfitRatio]).format(
-              '0,0.000',
+              "0,0.000"
             );
           } else {
             dataSources[`${item.calendarYear}-${item.period}`] = numeral(
-              item[field as keyof IProfitRatio],
-            ).format('0,0.000');
+              item[field as keyof IProfitRatio]
+            ).format("0,0.000");
           }
         });
         return dataSources;
       }),
-    [data, reportType, tabIndex],
+    [data, reportType, tabIndex]
   );
 
   return (
     <Stack rowGap={1}>
-      <TagCard tabs={['營運周轉', '固定資產周轉', '總資產周轉']} onChange={setTabIndex}>
+      {/* 資產的資料問題，先不做固定資產周轉與總資展週轉 */}
+      {/* <TagCard tabs={["營運周轉", "固定資產周轉", "總資產周轉"]} onChange={setTabIndex}> */}
+      <TagCard tabs={["營運周轉"]} onChange={setTabIndex}>
         <Stack
           direction="row"
           alignItems="center"
           sx={{
             mb: 3,
-            '&>button': {
+            "&>button": {
               mx: 1,
-              bgcolor: 'transparent',
+              bgcolor: "transparent",
               border: 0,
-              cursor: 'pointer',
+              cursor: "pointer",
             },
           }}
         >
@@ -131,7 +149,7 @@ export default function WeeklyTurnoverAbility() {
             <Button
               key={item.value}
               sx={{
-                color: item.value === period ? 'primary' : '#333',
+                color: item.value === period ? "primary" : "#333",
               }}
               onClick={() => setPeriod(item.value)}
             >
@@ -139,13 +157,20 @@ export default function WeeklyTurnoverAbility() {
             </Button>
           ))}
         </Stack>
-        <Box bgcolor="#fff"></Box>
+        <Box bgcolor="#fff">
+          <ReactChart
+            type="line"
+            data={TURNOVER_DATASETS[tabIndex.toString()]}
+            options={TURNOVER_GRAPH_OPTIONS[tabIndex.toString()]}
+            ref={chartRef}
+          />
+        </Box>
       </TagCard>
-      <TagCard tabs={['詳細數據']}>
+      <TagCard tabs={["詳細數據"]}>
         <Box
           className="ag-theme-alpine"
           style={{
-            paddingBottom: '24px',
+            paddingBottom: "24px",
           }}
         >
           <AgGridReact
