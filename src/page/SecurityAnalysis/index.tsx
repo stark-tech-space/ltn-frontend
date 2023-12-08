2e8c341d2a6f384003ee5b70c446768be2da73a4
王石川 <gavin@192.168.1.7>
2023-12-08 14:49:13 +0800

    Added:OperatingRatio

--- src/page/SecurityAnalysis/index.tsx
+++ src/page/SecurityAnalysis/index.tsx
@@ -3,14 +3,16 @@ import React, { useMemo, useState } from "react";
 import { SECURITY_PAGE_CONVERTER, SECURITY_PAGE_ENUM } from "types/security";
 import Developing from "component/Developing";
 import StructureRatio from "./component/StructureRatio";
+import QuickAndCurrentRatio from "./component/QuickAndCurrentRatio";
+import TimesInterestEarned from "./component/TimesInterestEarned";
 import PageNavigation from "component/PageNavigation";
-import OperatingNetIncomeRate from "./component/OperatingNetIncomeRate";
 import FlowRate from "./component/FlowRatio";
+import OperatingNetIncomeRate from "./component/OperatingNetIncomeRate";
 
 const CHILDREN_MAP: Record<SECURITY_PAGE_ENUM, React.ReactNode> = {
   [SECURITY_PAGE_ENUM.PAGE1]: <StructureRatio />,
-  [SECURITY_PAGE_ENUM.PAGE2]: <Developing />,
-  [SECURITY_PAGE_ENUM.PAGE3]: <Developing />,
+  [SECURITY_PAGE_ENUM.PAGE2]: <QuickAndCurrentRatio />,
+  [SECURITY_PAGE_ENUM.PAGE3]: <TimesInterestEarned />,
   [SECURITY_PAGE_ENUM.PAGE4]: <FlowRate />,
   [SECURITY_PAGE_ENUM.PAGE5]: <OperatingNetIncomeRate />,
   [SECURITY_PAGE_ENUM.PAGE6]: <Developing />,
