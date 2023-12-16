59f90e27da6cc8c9f8a58fe8ad9ef5b5ac87b966
Sam-nnnn <wwgod020590@gmail.com>
2023-12-16 13:32:18 +0800

    update income graph

--- src/page/Financial/component/IncomeStatement/IncomeGraph.tsx
+++ src/page/Financial/component/IncomeStatement/IncomeGraph.tsx
@@ -4,18 +4,30 @@ import { Box } from "@mui/material";
 import { OPTIONS } from "./GraphConfig";
 import { useRecoilValue } from "recoil";
 import { currentStock } from "recoil/selector";
-import { fetchIncomeStatement, fetchRevenue } from "api/financial";
-import moment from "moment";
+import {
+  fetchDanYiGongSiAnLi,
+  fetchIncomeStatement,
+  fetchRevenue,
+} from "api/financial";
+import moment, { unitOfTime } from "moment";
 import { IIncome } from "types/financial";
 import PeriodController from "component/PeriodController";
-import { getDataLimit } from "until";
-import { PERIOD } from "types/common";
-import { fetchGrowthRates } from "api/common";
+import {
+  caseDateToYYYYMMDD,
+  formatNumberFromCompanyCase,
+  genFullDateObject,
+} from "until";
 
 interface IGraphData {
   date: string;
   value: number;
 }
+
+const DEFAULT_PREV_DATA = {
+  sellingAndMarketingExpenses: 0,
+  generalAndAdministrativeExpenses: 0,
+  researchAndDevelopmentExpenses: 0,
+};
 
 interface IIncomeGraph {
   revenue: IGraphData[];
@@ -27,6 +39,15 @@ interface IIncomeGraph {
   incomeAfterTaxes: IGraphData[];
   equityAttributableToOwnersOfParent: IGraphData[];
 }
+
+const getPeriod: (date: string) => string | undefined = (date: string) => {
+  if (date.includes("Q1")) return "Q1";
+  if (date.includes("Q2")) return "Q2";
+  if (date.includes("Q3")) return "Q3";
+  if (date.includes("Q4")) return "Q4";
+};
+
+const INCOME_STATEMENT_NAME = "綜合損益表";
 
 const genStartDate = (years: number) => {
   return moment().subtract(years, "years").startOf("year").format("YYYY-MM-DD");
@@ -112,18 +133,20 @@ export default function IncomeGraph({
       });
 
       const data = await fetchExpenseData();
+      // console.log(data);
       if (data) {
         date.forEach((item, index) => {
           data.forEach((item2, index2) => {
-            if (item2.date === item) {
+            // console.log(item2.date, genFullDateObject(item).period);
+            if (item2.date === genFullDateObject(item).period) {
               sellingAndMarketingExpenses.push(
-                item2.sellingAndMarketingExpenses
+                item2.sellingAndMarketingExpenses.value
               );
               generalAndAdministrativeExpenses.push(
-                item2.generalAndAdministrativeExpenses
+                item2.generalAndAdministrativeExpenses.value
               );
               researchAndDevelopmentExpenses.push(
-                item2.researchAndDevelopmentExpenses
+                item2.researchAndDevelopmentExpenses.value
               );
             }
           });
@@ -149,9 +172,132 @@ export default function IncomeGraph({
   }, [stock.No, period, getGraphData]);
 
   const fetchExpenseData = async () => {
-    const limit = getDataLimit(PERIOD.QUARTER, period);
-    const rst = await fetchIncomeStatement(stock.Symbol, PERIOD.QUARTER, limit);
-    return rst;
+    const startDataMoment = moment().subtract(period, "year");
+
+    const res = await fetchDanYiGongSiAnLi({
+      securityCode: stock.No,
+      yearRange: `${startDataMoment.year()},${moment().year()}`,
+      size: Math.abs(startDataMoment.diff(moment(), "quarter")) - 1,
+    });
+
+    if (res) {
+      let prevData: { [key: string]: typeof DEFAULT_PREV_DATA } = {
+        Q1: { ...DEFAULT_PREV_DATA },
+        Q2: { ...DEFAULT_PREV_DATA },
+        Q3: { ...DEFAULT_PREV_DATA },
+      };
+
+      const expenseData = [...(res || [])?.list].reverse().map((item) => {
+        const incomeStatementTable = item.tables.find(
+          (item) => item.name === INCOME_STATEMENT_NAME
+        );
+        const date = item.id.slice(5);
+        const currentQuarter = item.quarter;
+
+        if (incomeStatementTable) {
+          let currentData: any = {};
+
+          const defaultData = incomeStatementTable.data
+            .map(({ date, ...data }) => ({
+              ...data,
+              start: caseDateToYYYYMMDD(date).start,
+              value: formatNumberFromCompanyCase(data.value) * 1000,
+              date,
+            }))
+            .sort((a, b) => (a.start > b.start ? -1 : 1));
+
+          defaultData.forEach((item) => {
+            if (
+              item.code === "6100" && // 銷售費用
+              !currentData.sellingAndMarketingExpenses
+            ) {
+              currentData.sellingAndMarketingExpenses = item;
+              if (currentQuarter !== "Q4") {
+                prevData[currentQuarter].sellingAndMarketingExpenses =
+                  item.value;
+              }
+            }
+            if (
+              item.code === "6200" && // 管理費用
+              !currentData.generalAndAdministrativeExpenses
+            ) {
+              currentData.generalAndAdministrativeExpenses = item;
+              if (currentQuarter !== "Q4") {
+                prevData[currentQuarter].generalAndAdministrativeExpenses =
+                  item.value;
+              }
+            }
+            if (
+              item.code === "6300" && // 研發費用
+              !currentData.researchAndDevelopmentExpenses
+            ) {
+              currentData.researchAndDevelopmentExpenses = item;
+              if (currentQuarter !== "Q4") {
+                prevData[currentQuarter].researchAndDevelopmentExpenses =
+                  item.value;
+              }
+            }
+          });
+
+          if (date === "2022-Q4") {
+            console.log(date, prevData);
+          }
+          if (currentQuarter === "Q4") {
+            const getPrevDataTotal = (
+              prevData: { [key in string]: typeof DEFAULT_PREV_DATA },
+              key: keyof typeof DEFAULT_PREV_DATA
+            ) => {
+              const Q1Data = prevData?.["Q1"]?.[key] || 0;
+              const Q2Data = prevData?.["Q2"]?.[key] || 0;
+              const Q3Data = prevData?.["Q3"]?.[key] || 0;
+              // console.log(date, key, Q1Data, Q2Data, Q3Data);
+              return Q1Data + Q2Data + Q3Data;
+            };
+
+            const q4Value = {
+              sellingAndMarketingExpenses: {
+                ...currentData["sellingAndMarketingExpenses"],
+                value:
+                  currentData?.["sellingAndMarketingExpenses"]?.value -
+                  getPrevDataTotal(prevData, "sellingAndMarketingExpenses"),
+              },
+              generalAndAdministrativeExpenses: {
+                ...currentData["generalAndAdministrativeExpenses"],
+                value:
+                  currentData?.["generalAndAdministrativeExpenses"]?.value -
+                  getPrevDataTotal(
+                    prevData,
+                    "generalAndAdministrativeExpenses"
+                  ),
+              },
+              researchAndDevelopmentExpenses: {
+                ...currentData["researchAndDevelopmentExpenses"],
+                value:
+                  currentData?.["researchAndDevelopmentExpenses"]?.value -
+                  getPrevDataTotal(prevData, "researchAndDevelopmentExpenses"),
+              },
+            };
+
+            currentData = {
+              ...currentData,
+              ...q4Value,
+            };
+
+            prevData = {
+              Q1: { ...DEFAULT_PREV_DATA },
+              Q2: { ...DEFAULT_PREV_DATA },
+              Q3: { ...DEFAULT_PREV_DATA },
+            };
+          }
+          return { ...currentData, date };
+        }
+        return null;
+      });
+
+      return [...expenseData].reverse().filter((item) => item?.date);
+    }
+    // const rst = await fetchIncomeStatement(stock.Symbol, PERIOD.QUARTER, limit);
+    // return rst;
   };
 
   useEffect(() => {
